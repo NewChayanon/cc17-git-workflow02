8db5530aa131f6bcea9f6f06d8b5fdd64a146ed8
newchayanon <new.chayanon20054@gmail.com>
2024-06-19 12:21:50 +0700

    create mw  not-found

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require('express')
+const notFound = require('./middlewares/not-found')
 
 const app = express()
+
+
+app.use(notFound)
 
 const port = 8000
 app.listen(8000,()=>console.log("Server on",port))
