e232179577cf9fee940b6c2f81d260acf1bb3385
klimprakhon <klimprakhon@gmail.com>
2024-06-19 14:22:09 +0700

    fix confilct between api

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -5,9 +5,7 @@ authRouter.get("/getUser");
 authRouter.post("/register", (req, res, next) => {
   res.json({ msg: "Register..." });
 });
-authRouter.post("/register", (req, res, next) => {
-  res.json({ msg: "Register..." });
-});
+
 authRouter.post("/login", (req, res, next) => {
   res.status(200).json({ msg: "login success" });
 });
