56b26b24e23add1506c2794c0c54d1b5cb1a750a
klimprakhon <klimprakhon@gmail.com>
2024-06-19 12:42:56 +0700

    change server

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-const express = require('express')
+const express = require("express");
 
-const app = express()
+const app = express();
 
-const port = 8000
-app.listen(8000,()=>console.log("Server on",port))
+const port = 8000;
+app.listen(8000, () => console.log("Server on", port));
